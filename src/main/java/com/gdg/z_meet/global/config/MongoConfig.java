b371e4ec1c12a5b142d510afd23f8b0be106ce6d
lsw71311 <lsw71311@naver.com>
2025-09-22 02:30:17 +0900

    [REFACTOR] MongoDB 저장시 _class 필드 자동 생성 방지

--- src/main/java/com/gdg/z_meet/global/config/MongoConfig.java
+++ src/main/java/com/gdg/z_meet/global/config/MongoConfig.java
@@ -1,18 +1,23 @@
 package com.gdg.z_meet.global.config;
 
 import com.mongodb.client.MongoClient;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import com.mongodb.client.MongoClients;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 // MongoDB 리포지토리만 활성화
 @Configuration
 @EnableMongoRepositories(basePackages = "com.gdg.z_meet.domain.chat.repository.mongo")
-public class MongoConfig {
+public class MongoConfig implements InitializingBean {
 
     @Value("${spring.data.mongodb.host}")
     private String mongoHost;
@@ -23,6 +28,10 @@ public class MongoConfig {
     @Value("${spring.data.mongodb.database}")
     private String databaseName;
 
+    @Autowired
+    @Lazy
+    private MappingMongoConverter mappingMongoConverter;
+
     @Bean
     public MongoClient mongoClient() {
         String connectionString = String.format("mongodb://%s:%d", mongoHost, mongoPort);
@@ -32,5 +41,10 @@ public class MongoConfig {
     @Bean
     public MongoTemplate mongoTemplate() {
         return new MongoTemplate(mongoClient(), databaseName);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        mappingMongoConverter.setTypeMapper(new DefaultMongoTypeMapper(null));
     }
 }
