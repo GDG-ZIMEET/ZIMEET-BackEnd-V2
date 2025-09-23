a03dd0d78859542d4d62f22ece541e8a924b3ef5
lsw71311 <lsw71311@naver.com>
2025-09-23 16:20:50 +0900

    [REFACTOR] convertor 주입 제거

--- src/main/java/com/gdg/z_meet/global/config/MongoDBConfig.java
+++ src/main/java/com/gdg/z_meet/global/config/MongoDBConfig.java
@@ -1,23 +1,17 @@
 package com.gdg.z_meet.global.config;
 
 import com.mongodb.client.MongoClient;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import com.mongodb.client.MongoClients;
-import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
-import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 // MongoDB 리포지토리만 활성화
 @Configuration
 @EnableMongoRepositories(basePackages = "com.gdg.z_meet.domain.chat.repository.mongo")
-public class MongoConfig implements InitializingBean {
+public class MongoDBConfig {
 
     @Value("${spring.data.mongodb.host}")
     private String mongoHost;
@@ -28,9 +22,9 @@ public class MongoConfig implements InitializingBean {
     @Value("${spring.data.mongodb.database}")
     private String databaseName;
 
-    @Autowired
-    @Lazy
-    private MappingMongoConverter mappingMongoConverter;
+//    @Autowired
+//    @Lazy
+//    private MappingMongoConverter mappingMongoConverter;
 
     @Bean
     public MongoClient mongoClient() {
@@ -43,8 +37,8 @@ public class MongoConfig implements InitializingBean {
         return new MongoTemplate(mongoClient(), databaseName);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        mappingMongoConverter.setTypeMapper(new DefaultMongoTypeMapper(null));
-    }
+//    @Override
+//    public void afterPropertiesSet() throws Exception {
+//        mappingMongoConverter.setTypeMapper(new DefaultMongoTypeMapper(null));
+//    }
 }
