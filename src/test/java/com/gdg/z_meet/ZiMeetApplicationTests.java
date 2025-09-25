a5436e12133626196e961d84d245b7c168e4f451
parkmineum <a01087164428@gmail.com>
2025-09-25 18:24:44 +0900

    [REFACTOR] : test.yml 추가

--- src/test/java/com/gdg/z_meet/ZiMeetApplicationTests.java
+++ src/test/java/com/gdg/z_meet/ZiMeetApplicationTests.java
@@ -2,12 +2,13 @@ package com.gdg.z_meet;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ZiMeetApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
