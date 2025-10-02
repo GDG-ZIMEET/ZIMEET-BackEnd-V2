6e324ba7cf27aee0b55fa770611c316e64a03591
parkmineum <a01087164428@gmail.com>
2025-10-02 13:03:44 +0900

    [FIX] : 쓰기 락을 통한 동일 트랜잭션 내 Race Condition 방지

--- src/main/java/com/gdg/z_meet/domain/fcm/repository/FcmTokenRepository.java
+++ src/main/java/com/gdg/z_meet/domain/fcm/repository/FcmTokenRepository.java
@@ -2,8 +2,11 @@ package com.gdg.z_meet.domain.fcm.repository;
 
 import com.gdg.z_meet.domain.fcm.entity.FcmToken;
 import com.gdg.z_meet.domain.user.entity.User;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,6 +22,10 @@ public interface FcmTokenRepository extends JpaRepository<FcmToken, Long> {
 
     @Query(" SELECT ft FROM FcmToken ft JOIN FETCH ft.user u WHERE u.pushAgree = true")
     List<FcmToken> findAllByUserPushAgreeTrue();
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT f FROM FcmToken f WHERE f.user = :user")
+    Optional<FcmToken> findByUserForUpdate(@Param("user") User user);
 
     void deleteByUser(User user);
 
