06260f1a908333c903fc8566f2c7f79f6d974c63
parkmineum <a01087164428@gmail.com>
2026-01-31 00:31:20 +0900

    [REFACTOR] : RabbitMQ 백프레셔 추가

--- src/main/java/com/gdg/z_meet/global/config/RabbitMqConfig.java
+++ src/main/java/com/gdg/z_meet/global/config/RabbitMqConfig.java
@@ -5,6 +5,7 @@ import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -114,5 +115,23 @@ public class RabbitMqConfig {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(messageConverter());
         return rabbitTemplate;
+    }
+
+    /**
+     * 컨슈머 백프레셔 및 재시도 설정
+     */
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(messageConverter());
+
+        // 백프레셔: 한 번에 가져올 메시지 수 제한 (DB 및 FCM API 부하 조절)
+        factory.setPrefetchCount(20);
+
+        // 재시도 정책 설정
+        // 여기서 재시도는 'Consumer 애플리케이션 내'에서의 재시도입니다.
+        // 최종 실패 시 DLQ로 보내기 위해 RejectAndDontRequeueRecoverer 사용 가능
+        return factory;
     }
 }
