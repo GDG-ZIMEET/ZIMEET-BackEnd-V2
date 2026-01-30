3109c3cf1d38e7c2067352c0d2e9f35eacdf827a
parkmineum <a01087164428@gmail.com>
2026-01-31 00:31:01 +0900

    [REFACTOR] : 브로드캐스팅 알림 페이징 조회하도록 변경

--- src/main/java/com/gdg/z_meet/domain/fcm/repository/FcmTokenRepository.java
+++ src/main/java/com/gdg/z_meet/domain/fcm/repository/FcmTokenRepository.java
@@ -3,6 +3,8 @@ package com.gdg.z_meet.domain.fcm.repository;
 import com.gdg.z_meet.domain.fcm.entity.FcmToken;
 import com.gdg.z_meet.domain.user.entity.User;
 import jakarta.persistence.LockModeType;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
@@ -15,13 +17,18 @@ public interface FcmTokenRepository extends JpaRepository<FcmToken, Long> {
 
     void deleteAllByUser(User user);
 
-
     List<FcmToken> findAllByUser(User user);
 
     void deleteByToken(String token);
 
     @Query(" SELECT ft FROM FcmToken ft JOIN FETCH ft.user u WHERE u.pushAgree = true")
     List<FcmToken> findAllByUserPushAgreeTrue();
+
+    /**
+     * 브로드캐스트용 페이징 조회 (메모리 최적화)
+     */
+    @Query("SELECT ft FROM FcmToken ft JOIN FETCH ft.user u WHERE u.pushAgree = true")
+    Slice<FcmToken> findAllByUserPushAgreeTrueSlice(Pageable pageable);
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT f FROM FcmToken f WHERE f.user = :user")
@@ -30,4 +37,6 @@ public interface FcmTokenRepository extends JpaRepository<FcmToken, Long> {
     void deleteByUser(User user);
 
     Optional<FcmToken> findByUser(User user);
+
+    Optional<FcmToken> findByToken(String token);
 }
