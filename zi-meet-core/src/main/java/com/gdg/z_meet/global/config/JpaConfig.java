36bcd9fc32e0fe33441d39fa5ece51906b57c890
parkmineum <a01087164428@gmail.com>
2026-02-05 17:12:16 +0900

    [REFACTOR] : K6 테스트 관련 JPA 설정 추

--- zi-meet-core/src/main/java/com/gdg/z_meet/global/config/JpaConfig.java
+++ zi-meet-core/src/main/java/com/gdg/z_meet/global/config/JpaConfig.java
@@ -1,15 +1,19 @@
 package com.gdg.z_meet.global.config;
 
 import com.gdg.z_meet.domain.chat.repository.mongo.MessageRepository;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@EnableJpaRepositories(
-        basePackages = "com.gdg.z_meet.domain",
-        excludeFilters = @Filter(type = FilterType.ASSIGNABLE_TYPE, value = MessageRepository.class) // MongoDB 리포지토리 제외
+@EnableJpaAuditing
+@EntityScan(basePackages = "com.gdg.z_meet.domain")
+@EnableJpaRepositories(basePackages = "com.gdg.z_meet.domain", excludeFilters = @Filter(type = FilterType.ASSIGNABLE_TYPE, value = MessageRepository.class) // MongoDB
+                                                                                                                                                            // 리포지토리
+                                                                                                                                                            // 제외
 )
 public class JpaConfig {
     // JPA 관련 설정
